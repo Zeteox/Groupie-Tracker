4c6623de8bc9e01bef557bc46cfeaf257007362f
Delprat Loïc <loic.delprat@ynov.com>
2024-12-19 13:44:01 +0100

    feat(webserver): created a webserver to host the website

---
+++ Cmd/Main.go
@@ -0,0 +1,7 @@
+package main
+
+import "GroupieTracker/Internal/Server"
+
+func main() {
+	internal.CreateAndListenServer(8080)
+}
---
+++ Internal/Server/Server.go
@@ -0,0 +1,18 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func CreateAndListenServer(port int) {
+	//Creation of routes for all pages
+	//http.HandleFunc("/", nil)
+
+	//Creation of route for statics files
+
+	//Creating a server and listening on the given port
+	fmt.Println("\u001B[36m[INFO]\u001B[0m Listening on port " + strconv.Itoa(port) + " (http://localhost:" + strconv.Itoa(port) + ")")
+	http.ListenAndServe("localhost:"+strconv.Itoa(port), nil)
+}
