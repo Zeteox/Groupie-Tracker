d7815ba75dd5652d185720a23861f740d443f41b
Delprat Loïc <loic.delprat@ynov.com>
2024-12-20 14:42:29 +0100

    done the handler for the index page and tested showing some data

--- Internal/Server/Server.go
+++ Internal/Server/Server.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"GroupieTracker/Internal/Server/Handlers"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -8,12 +9,12 @@ import (
 
 func CreateAndListenServer(port int) {
 	//Creation of routes for all pages
-	//http.HandleFunc("/", nil)
+	http.HandleFunc("/", Handlers.HandlerIndexPage)
 
 	//Creation of route for statics files
 	fs := http.FileServer(http.Dir("./Frontend/Static"))
 	http.Handle("/Static/", http.StripPrefix("/Static/", fs))
-	
+
 	//Creating a server and listening on the given port
 	fmt.Println("\u001B[36m[INFO]\u001B[0m Listening on port " + strconv.Itoa(port) + " (http://localhost:" + strconv.Itoa(port) + ")")
 	http.ListenAndServe("localhost:"+strconv.Itoa(port), nil)
