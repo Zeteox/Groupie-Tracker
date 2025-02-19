761c1defb7530817b4898707453f80290146782c
Robl Elisabeth <elisabeth.robl@ynov.com>
2025-02-19 15:20:32 +0100

    feat(InfosPage): added dates and locations of concerts

--- Internal/Server/Handlers/HandlerInfosPage.go
+++ Internal/Server/Handlers/HandlerInfosPage.go
@@ -7,11 +7,36 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 func HandlerInfosPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	templ, err := template.ParseFiles("./Frontend/Templates/infos.gohtml")
+	templ, err := template.New("infos.gohtml").Funcs(template.FuncMap{
+		"CityWithCountry": func(cityWithCountry string) []string {
+			split := strings.Split(cityWithCountry, "-")
+			citySplit := strings.Split(split[0], "_")
+			countrySplit := strings.Split(split[1], "_")
+
+			city := strings.ToUpper(citySplit[0][:1]) + citySplit[0][1:]
+			if len(citySplit) > 1 {
+				for i := 1; i <= len(citySplit)-1; i++ {
+					city += " " + strings.ToUpper(citySplit[i][:1]) + citySplit[i][1:]
+
+				}
+			}
+
+			country := strings.ToUpper(countrySplit[0][:1]) + countrySplit[0][1:]
+			if len(countrySplit) > 1 {
+				for i := 1; i <= len(countrySplit)-1; i++ {
+					country += " " + strings.ToUpper(countrySplit[i][:1]) + countrySplit[i][1:]
+
+				}
+			}
+
+			return []string{city, country}
+		},
+	}).ParseFiles("./Frontend/Templates/infos.gohtml")
 	if err != nil {
 		fmt.Println("\033[31m[ERR]\033[0m [ROUTAGE] Error occured when handling index page request: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -24,6 +49,12 @@ func HandlerInfosPage(w http.ResponseWriter, r *http.Request) {
 	param, _ := url.ParseQuery(myUrl.RawQuery)
 	IdStr := param.Get("Id")
 	Id, _ := strconv.Atoi(IdStr)
+
+	if Id < 1 {
+		Id = 1
+	} else if Id > 52 {
+		Id = 52
+	}
 
 	data, err := Utils.GetArtistsById(Id)
 	if err != nil {
